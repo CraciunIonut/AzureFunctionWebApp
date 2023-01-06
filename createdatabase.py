b2e1d1662be829aba665ef877786646da4233f95
CraciunIonut <alexionutcraciun@gmail.com>
2023-01-06 17:46:59 +0200

    Removed unnecesary files

--- createdatabase.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-def createdb():
-    connection = sqlite3.connect('db/users.db')
-    cursor = connection.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS Users 
-    (id integer primary key autoincrement, name text, password text)''')
-    connection.close()
